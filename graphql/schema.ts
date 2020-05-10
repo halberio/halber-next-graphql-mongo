15ad1ae8808868bfa3d90f55c05ca3214e2915a4
h0mdi <hamdijomaa.x@gmail.com>
2020-05-10 01:11:46 +0100

    just backup

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,4 +1,44 @@
-import { makeExecutableSchema } from "graphql-tools";
-import { typeDefs } from "./type-defs";
-import { resolvers } from "./resolvers";
-export const schema = makeExecutableSchema({ typeDefs, resolvers });
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+  type User {
+    id: ID
+    name: String!
+    email: String!
+  }
+  type AuthData {
+    userId: ID!
+    token: String!
+    tokenExpiration: Int!
+  }
+  input SignUpInput {
+    name: String!
+    email: String!
+    password: String!
+  }
+  input SignInInput {
+    email: String!
+    password: String!
+  }
+
+  type Query {
+    user(id: ID!): User!
+    currentUser: User
+    users: [User]!
+    viewer: User
+    posts(first: Int = 25, skip: Int = 0): [Post!]!
+  }
+  type AuthPayload {
+    user: User
+  }
+  type Mutation {
+    signUp(input: SignUpInput): User!
+    login(email: String!, password: String!): AuthPayload
+    signOut: Boolean!
+    createPost(name: String): Post!
+  }
+  type Post {
+    id: ID!
+    name: String!
+  }
+`;
